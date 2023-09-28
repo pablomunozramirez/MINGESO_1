003b4a54f29846e2d31a83145596a39786c67ac3
pablomunozramirez <pablo.munoz.ra@usach.cl>
2023-09-27 23:33:36 -0300

    segundo commit

---
+++ PEP1MINGESO/src/main/java/com/example/PEP1MINGESO/Pep1MingesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.PEP1MINGESO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pep1MingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pep1MingesoApplication.class, args);
+	}
+
+}
---
+++ PEP1MINGESO/src/test/java/com/example/PEP1MINGESO/Pep1MingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PEP1MINGESO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pep1MingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
